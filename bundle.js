5ebfa0a487facfc82d50939d83f55d50b3401af7
Marshall Quander <marshall@quander.me>
2018-01-19 16:26:06 -0800

    Add ability to subscribe to Janus events

--- bundle.js
+++ bundle.js
@@ -25,6 +25,17 @@ JanusPluginHandle.prototype.detach = function() {
   return this.send("detach");
 };
 
+/** Registers a callback to be fired upon the reception of any incoming Janus signals for this plugin handle with the
+ * `janus` attribute equal to `ev`.
+ **/
+JanusPluginHandle.prototype.on = function(ev, callback) {
+  return this.session.on(ev, signal => {
+    if (signal.sender == this.id) {
+      callback(signal);
+    }
+  });
+};
+
 /**
  * Sends a signal associated with this handle. Signals should be JSON-serializable objects. Returns a promise that will
  * be resolved or rejected when a response to this signal is received, or when no response is received within the
@@ -60,6 +71,7 @@ function JanusSession(output, options) {
   this.id = undefined;
   this.nextTxId = 0;
   this.txns = {};
+  this.eventHandlers = {};
   this.options = Object.assign({
     verbose: false,
     timeoutMs: 10000,
@@ -90,6 +102,17 @@ JanusSession.prototype.isError = function(signal) {
   return signal.janus === "error";
 };
 
+/** Registers a callback to be fired upon the reception of any incoming Janus signals for this session with the
+ * `janus` attribute equal to `ev`.
+ **/
+JanusSession.prototype.on = function(ev, callback) {
+  var handlers = this.eventHandlers[ev];
+  if (handlers == null) {
+    handlers = this.eventHandlers[ev] = [];
+  }
+  handlers.push(callback);
+};
+
 /**
  * Callback for receiving JSON signalling messages pertinent to this session. If the signals are responses to previously
  * sent signals, the promises for the outgoing signals will be resolved or rejected appropriately with this signal as an
@@ -105,16 +128,24 @@ JanusSession.prototype.receive = function(signal) {
   if (signal.session_id != this.id) {
     console.warn("Incorrect session ID received in Janus signalling message: was " + signal.session_id + ", expected " + this.id + ".");
   }
+
+  var responseType = signal.janus;
+  var handlers = this.eventHandlers[responseType];
+  if (handlers != null) {
+    for (var i = 0; i < handlers.length; i++) {
+      handlers[i](signal);
+    }
+  }
+
   if (signal.transaction != null) {
     var txn = this.txns[signal.transaction];
-
     if (txn == null) {
       // this is a response to a transaction that wasn't caused via JanusSession.send, or a plugin replied twice to a
       // single request, or something else that isn't under our purview; that's fine
       return;
     }
 
-    if (signal.janus === "ack" && txn.type == "message") {
+    if (responseType === "ack" && txn.type == "message") {
       // this is an ack of an asynchronously-processed plugin request, we should wait to resolve the promise until the
       // actual response comes in
       return;
